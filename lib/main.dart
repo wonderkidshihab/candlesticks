bbdc53972a90fead0f277ceb7508580d35c8ac59
Shihab Uddin <contact.shihab.247@gmail.com>
2024-01-02 01:41:04 +0600

    Add gold price API integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:developer';
 
+import 'package:candlesticks/candlesticks.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:candlesticks/candlesticks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +28,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -36,7 +37,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<Candle> candles = [];
   bool themeIsDark = false;
 
@@ -46,17 +46,14 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  Future<List<Candle>> fetchCandles() async {
-    final uri = Uri.parse(
-        "https://api.binance.com/api/v3/klines?symbol=BNBUSDT&interval=1h&limit=100");
-    final res = await http.get(uri);
-    print(res.body);
-    print("-------------------------");
-    return (jsonDecode(res.body) as List<dynamic>)
-        .map((e) => Candle.fromJson(e))
-        .toList()
-        .reversed
-        .toList();
+  Future<Candle> fetchCandles() async {
+    final uri = Uri.parse("https://www.goldapi.io/api/XAU/USD");
+    final res = await http.get(uri, headers: {
+      "x-access-token": "goldapi-xqk5prlqvb2tu9-io",
+      "Content-Type": "application/json",
+    });
+    log(res.body);
+    return fromGoldApi(jsonDecode(res.body));
   }
 
   @override
@@ -73,13 +70,57 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
   fetch() async {
-    fetchCandles().then((value) {
+    Timer.periodic(const Duration(seconds: 5), (timer) async {
+      final candle = await fetchCandles();
       setState(() {
-        candles = value;
+        candles.add(candle);
       });
     });
-    await Future.delayed(Duration(seconds: 2));
-    fetch();
   }
+}
+//             1704137547500
+// "timestamp":1704136846
+// "metal":"XAU"
+// "currency":"USD"
+// "exchange":"FOREXCOM"
+// "symbol":"FOREXCOM:XAUUSD"
+// "prev_close_price":2065.62
+// "open_price":2065.62
+// "low_price":2058.29
+// "high_price":2074.86
+// "open_time":1703808000
+// "price":2062.88
+// "ch":-2.74
+// "chp":-0.13
+// "ask":2063.24
+// "bid":2062.53
+// "price_gram_24k":66.3231
+// "price_gram_22k":60.7962
+// "price_gram_21k":58.0327
+// "price_gram_20k":55.2693
+// "price_gram_18k":49.7423
+// "price_gram_16k":44.2154
+// "price_gram_14k":38.6885
+// "price_gram_10k":27.6346
+
+//   Candle({
+//   required this.date,
+//   required this.high,
+//   required this.low,
+//   required this.open,
+//   required this.close,
+//   required this.volume,
+// });
+
+Candle fromGoldApi(Map<String, dynamic> json) {
+  return Candle(
+    date: DateTime.fromMicrosecondsSinceEpoch((int.parse(json['open_time'].toString()) * 100)),
+    high: json['high_price'],
+    low: json['low_price'],
+    open: json['open_price'],
+    close: json['price'],
+    volume: 5,
+  );
 }
